601bfd42dc15be64c852a940e2ff09a45bab841f
Benjamin Ek <benjamin.ek1@learnet.se>
2019-05-21 15:41:43 +0200

    asd

--- ShootEmUp/Player.cs
+++ ShootEmUp/Player.cs
@@ -50,7 +50,7 @@ namespace ShootEmUp
         {
             myPosition = myStartPos;
             myAnimationTimer = myAnimationCooldown;
-            myTexture = Customization.GetSelectedTexture();
+            //myTexture = Customization.GetSelectedTexture();
             myRectangle = CreateRectangle();
             myRectangle.Size = new Point((int)(Game1.myShip.Bounds.Size.X * myScale), (int)(Game1.myShip.Bounds.Size.Y * myScale));
             myAttackCooldown = myOriginalAttackCooldown;
